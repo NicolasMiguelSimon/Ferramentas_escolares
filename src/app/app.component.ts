d8b7507987a23bbcde2efbbafd9cdd2a6f0ceefb
NicolasMiguelSimon <nicolasximom11@gmail.com>
2024-07-11 23:16:41 -0300

    routes adicionado, correcoes no layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from "./about/about.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
